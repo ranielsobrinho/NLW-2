7903b0244d27ce6ef15189a48779ec65863b5ff8
Raniel <raniel25sobrinho@gmail.com>
2020-08-13 16:18:11 -0300

    Feito refatoração do arquivo para ficarem apenas coisas relacionadas ao servidor

--- src/server.js
+++ src/server.js
@@ -1,30 +1,7 @@
-const { subjects, weekdays, getSubject } = require('./utils/format')
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", {filters, proffys, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-
-    if (isNotEmpty) {
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
+//servidor
 const express = require('express')
 const server = express()
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 //configurar o nunjucks
 const nunjucks = require('nunjucks')
@@ -33,12 +10,16 @@ nunjucks.configure('src/views', {
     noCache : true,
 })
 
-server.use(express.static("public")) //configurar arquivos estáticos
-
+//Início e configuração do servidor
+server
+//receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+//configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+//rotas da aplicação
 .get('/', pageLanding)
-
 .get('/study', pageStudy)
-
 .get('/give-classes', pageGiveClasses)
-
+.post('/save-classes', saveClasses)
+// start do servidor
 .listen(5500)
